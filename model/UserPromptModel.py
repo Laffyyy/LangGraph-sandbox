4b130c6fa117474212a1fa03d0cc6def9cced0e1
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2025-07-22 14:49:09 +0800

    remove trace route

--- model/UserPromptModel.py
+++ model/UserPromptModel.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel, Field
-from langsmith import traceable
 
-@traceable
 class UserPrompt(BaseModel):
     user_prompt: str = Field(description="The user's prompt")
     better_prompt: str = Field(description="The better prompt")
